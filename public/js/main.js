48c18d05c0b19c78df6df9c17b13d0f5bfce20c3
GAUTHIER <gauthierdewit@gmail.com>
2021-02-26 16:27:00 +0100

    Exo03 fini

--- public/js/main.js
+++ public/js/main.js
@@ -10,6 +10,7 @@ egale0.addEventListener("click", () => {
     reponse0.innerText = parseInt(valeur0.value) + parseInt(valeur1.value)
 
 })
+//--------------------------------------------
 
 // Exo 02
 let boxBody1 = document.querySelectorAll(".box-body")[1]
@@ -17,6 +18,7 @@ let valeur2 = boxBody1.querySelectorAll("input")[0]
 let valeur3 = boxBody1.querySelectorAll("input")[1]
 let egale1 = boxBody1.querySelectorAll(".btn")[1]
 let reponse1 = boxBody1.querySelector("span")
+
 let select0 = boxBody1.querySelector("select")
 
 
@@ -37,5 +39,79 @@ egale1.addEventListener("click", () => {
     }
 
 })
+//-------------------------------------------
 
 // Exo 03
+let boxBody2 = document.querySelectorAll(".box-body")[2]
+let valeur4 = boxBody2.querySelectorAll("input")[0]
+let valeur5 = boxBody2.querySelectorAll("input")[1]
+let egale2 = boxBody2.querySelectorAll(".btn")[1]
+let reponse2 = boxBody2.querySelectorAll("span")[1]
+
+// Button
+let addition = boxBody2.querySelectorAll("button")[1]
+let soustraction = boxBody2.querySelectorAll("button")[2]
+let multiplication = boxBody2.querySelectorAll("button")[3]
+let division = boxBody2.querySelectorAll("button")[4]
+
+//Symbol
+let i = boxBody2.querySelectorAll("span")[0]
+
+// Addition
+addition.addEventListener("click", () => {
+    reponse2.innerText = parseInt(valeur4.value) + parseInt(valeur5.value)
+    i.querySelector("i").className = addition.querySelector("i").className
+})
+// Soustraction
+soustraction.addEventListener("click", () => {
+    reponse2.innerText = parseInt(valeur4.value) - parseInt(valeur5.value)
+    i.querySelector("i").className = soustraction.querySelector("i").className
+})
+// Multiplication
+multiplication.addEventListener("click", () => {
+    reponse2.innerText = parseInt(valeur4.value) * parseInt(valeur5.value)
+    i.querySelector("i").className = multiplication.querySelector("i").className
+})
+// Division
+division.addEventListener("click", () => {
+    reponse2.innerText = parseInt(valeur4.value) / parseInt(valeur5.value)
+    i.querySelector("i").className = division.querySelector("i").className
+})
+//-----------------------------------------
+
+//Exo 04
+let boxBody2 = document.querySelectorAll(".box-body")[2]
+let valeur4 = boxBody2.querySelectorAll("input")[0]
+let valeur5 = boxBody2.querySelectorAll("input")[1]
+let egale2 = boxBody2.querySelectorAll(".btn")[1]
+let reponse2 = boxBody2.querySelectorAll("span")[1]
+
+// Button
+let addition = boxBody2.querySelectorAll("button")[1]
+let soustraction = boxBody2.querySelectorAll("button")[2]
+let multiplication = boxBody2.querySelectorAll("button")[3]
+let division = boxBody2.querySelectorAll("button")[4]
+
+//Symbol
+let i = boxBody2.querySelectorAll("span")[0]
+
+// Addition
+addition.addEventListener("click", () => {
+    reponse2.innerText = parseInt(valeur4.value) + parseInt(valeur5.value)
+    i.querySelector("i").className = addition.querySelector("i").className
+})
+// Soustraction
+soustraction.addEventListener("click", () => {
+    reponse2.innerText = parseInt(valeur4.value) - parseInt(valeur5.value)
+    i.querySelector("i").className = soustraction.querySelector("i").className
+})
+// Multiplication
+multiplication.addEventListener("click", () => {
+    reponse2.innerText = parseInt(valeur4.value) * parseInt(valeur5.value)
+    i.querySelector("i").className = multiplication.querySelector("i").className
+})
+// Division
+division.addEventListener("click", () => {
+    reponse2.innerText = parseInt(valeur4.value) / parseInt(valeur5.value)
+    i.querySelector("i").className = division.querySelector("i").className
+})
